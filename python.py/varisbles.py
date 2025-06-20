24e23a628abf231af4936de9b6f9fa06d47b41c6
apsara208 <apsarachhetri40@gmail.com>
2025-06-20 12:41:20 +0000

    variables in python

---
+++ python.py/varisbles.py
@@ -0,0 +1,34 @@
+#aassigning a value to a variables 
+'''dynamically typed lanaguage:type checking is performed at runtime rather than at compile time'''
+r=int(input('input a number'))
+print(r)
+#indexing
+a='apsara'
+print(a[-1])
+#slicing (exclusive of last argument)
+b="chhetri"
+print(b[-5:-1:2]) #slicimg is always done left to right
+
+
+
+
+#DATA TYPES IN PYTHON
+'''PRIMTIVE:
+int
+string 
+float
+boolean 
+complex
+
+
+NON-PRIMITIVE:
+array:same data type [,]
+list : can be different data types in one [,] 
+tuple:immutable(,)
+dictionary :{:,:} key value pair    d={}
+set : cannot have repeating values{,} , unordered , no indexing (subset , set , superset)   set=set()
+
+
+SPECIAL :
+None'''
+# implicit(direct) and explicit(uses int()) type datat type conversion
