7f94dc28adb2c4d58e0a7c2536f1d447e2b11630
apsara208 <apsarachhetri40@gmail.com>
2025-06-21 12:17:28 +0000

    for loo

--- python.py/loops.py
+++ python.py/loops.py
@@ -1,4 +1,17 @@
 #FOR LOOP 
-for x in range(1,5):
+for x in range(1,5): #(start,stop,skip)
     print(x) #inside the loop
 print("happy") #outside the loop
+
+#roll=123490877
+#for i in roll: # int is not iterable
+ #   print(i)
+
+#whereas
+rolln="123456789"
+for y in rolln:
+    print(y)
+
+
+ #continue: skips over an iteration and jumps to next
+ # break: terminates the iteration   
