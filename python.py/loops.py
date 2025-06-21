5207df378212f17aa20fbd1d9c3e8894d2d4ad6a
apsara208 <apsarachhetri40@gmail.com>
2025-06-21 12:38:05 +0000

    for and while loop

--- python.py/loops.py
+++ python.py/loops.py
@@ -1,17 +1,27 @@
 #FOR LOOP 
-for x in range(1,5): #(start,stop,skip)
+'''for x in range(1,5): #(start,stop,skip)
     print(x) #inside the loop
-print("happy") #outside the loop
+print("happy") #outside the loop '''
 
 #roll=123490877
 #for i in roll: # int is not iterable
  #   print(i)
 
 #whereas
-rolln="123456789"
+'''rolln="123456789"
 for y in rolln:
-    print(y)
+    print(y)'''
 
 
  #continue: skips over an iteration and jumps to next
  # break: terminates the iteration   
+
+#while loop:executes code while some condition remains true
+'''
+name=input("enter your name")
+while name=="":
+   print("you did nto enter your name")
+   name=input("enter your name")
+print(f"hello {name}") '''
+
+ 
