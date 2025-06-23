d806f4ddb52ef613592ce32734fc2099ab1888b4
apsara208 <apsarachhetri40@gmail.com>
2025-06-23 11:34:36 +0000

    numpy and jupyter notebook defined

---
+++ python.py/numpy.py
@@ -0,0 +1,8 @@
+#NumPy is a powerful Python library primarily used for working with arrays.It is the fundamental package for scientific computing in Python, providing support for large, multi-dimensional arrays and matrices
+#numpy (why?):
+# efficient storage
+# provides better way of handling data for processing
+# fast
+#easy to learn
+# uses relatively less memory.
+#Jupyter Notebook is an open-source web application that allows you to create and share documents that contain live code, equations, visualizations, and narrative tex
