0f10f977c247e0f73dd90e4929e02fbfb7ae9338
apsara208 <apsarachhetri40@gmail.com>
2025-09-12 07:45:42 +0000

    unversal functions

---
+++ numpyy.py/UFunc.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+#numpy universal functions
+np1=np.array([0,1,2,3,4,5,6,7,-8,-9])
+#print(np1)
+#square root of each element
+#print(np.sqrt(np1))
+#absolute value
+print(np.absolute(np1))
+#exponents
+print(np.exp(np1))
+#max/ min
+print(np.max(np1))
+print(np.min(np1))
+#sign positive or negative 
+print(np.sign(np1))
+#[ 0  1  1  1  1  1  1  1 -1 -1]
+#trig sin cos log
+print(np.log(np1))
+#universal functions doc-Numpy.orgn (also math operators)
