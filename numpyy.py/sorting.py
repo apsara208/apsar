460b02cbab41c0558fe3c11a718c34debc3233a6
apsara208 <apsarachhetri40@gmail.com>
2025-09-15 09:49:06 +0000

    sorting of numpy arrays

---
+++ numpyy.py/sorting.py
@@ -0,0 +1,24 @@
+import numpy as np
+#sorted is the copy
+#np.sort()   numerical
+'''=np.array([3,4,7,2,6,0,21,7,4])
+print(np.sort(np1))'''
+
+#alphabetical
+'''
+np2=np.array(["jon","tam","polly"])
+print(np2)
+print(np.sort(np2))'''
+
+
+#boolean t/f
+'''
+np3=np.array([True,False,False,True])
+print(np.sort(np3))
+'''
+
+
+#2d
+np4=np.array([[1,26,3],[7,3,0]])
+print(np.sort(np4))
+#sorts their individual1d arrays
