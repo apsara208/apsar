e653e0e67614d9e1cda61a608325b53709cb1cbd
apsara208 <apsarachhetri40@gmail.com>
2025-12-07 17:15:23 +0000

    kinds of plot

--- pandass.py/plot.py
+++ pandass.py/plot.py
@@ -3,6 +3,19 @@ import pandas as pd
 #We can use Pyplot, a submodule of the Matplotlib library to visualize the diagram on the screen.
 import matplotlib.pyplot as plt
 my_df=pd.read_csv("sales.csv")
-my_df.plot()
+#my_df.plot()
+#my_df.plot(kind="scatter",x="Sales",y="Expenses")
+my_df["Sales"].plot(kind="hist")
 #plt.show()
+
 plt.savefig("plot.png")
+'''kind value	What it shows
+line	Line graph ✅ (default)
+bar	Vertical bar chart
+barh	Horizontal bar chart
+hist	Histogram
+box	Box plot
+kde	Density curve
+area	Area chart
+pie	Pie chart
+scatter	Scatter plot'''
