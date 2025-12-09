cc5f3f1c7b488e3a706a0d11586f87d51a53ec87
apsara208 <apsarachhetri40@gmail.com>
2025-12-09 06:28:29 +0000

    labels

---
+++ matplot.py/labels.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+#labels for x axis and y axis 
+#title for the representation 
+#fonts for title and labels
+#position the title (loc)
+a=np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+b=np.array([240, 250, 260, 210, 250, 290, 300, 210, 320, 330])
+plt.plot(a,b,c="yellow")
+font1={"family":"serif","color":"blue","size":20}
+font2={"family":"serif","color":"darkred","size":15}
+plt.xlabel("average pulse",fontdict=font1)
+plt.ylabel("calorie burnage",fontdict=font1)
+plt.title("sports watch data",fontdict=font2,loc="left")
+
+plt.savefig("pics3.png")
